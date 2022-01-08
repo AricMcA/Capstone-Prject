438b7ebdec109c3294f0a407fb6b23abe433e8b1
Aric McAlister <aricmca@gmail.com>
2022-01-08 14:33:17 -0600

    added Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Home from './components/Home';
+import NavBar from './components/NavBar';
 import { Routes, Route, BrowserRouter as Router} from 'react-router-dom';
+import { UserProvider } from './Context/user'
 
 function App() {
   return (
